8bd617c9fa81fdf917fdf97cc3b76700d9c84b62
kapil <kapilrajkc10@gmail.com>
2025-07-30 14:32:30 +0545

    Update Api.jsx

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://income-backend-2.onrender.com",
+  baseURL: "https://income-server-2.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
